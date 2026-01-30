4a60ba660070c06ee9c06dad35a3d4d89e09fffa
AdhamG <adhamgaweesh@gmail.com>
2026-01-30 15:45:46 +0200

    added flat shading

--- src/Canvas.java
+++ src/Canvas.java
@@ -11,6 +11,7 @@ public class Canvas extends JPanel{
     private int rotX = 0, rotY = 0, rotZ = 0;
     private boolean showWireFrame = false;
     private BufferedImage img = null;
+    private Vertex lightDir = new Vertex(0, 5, -1, 0).normalize();
 
     public Canvas(Mesh mesh, Camera camera) {
         setBackground(Color.GRAY);
@@ -92,25 +93,30 @@ public class Canvas extends JPanel{
         Arrays.stream(zBuffer).forEach(y -> Arrays.fill(y, Double.POSITIVE_INFINITY));
 
         for (Triangle tri : mesh.getTris()) {
-            Vertex v1 = MV.multiply(tri.getV1());
-            Vertex v2 = MV.multiply(tri.getV2());
-            Vertex v3 = MV.multiply(tri.getV3());
+            // View space vertices
+            Vertex v1View = MV.multiply(tri.getV1());
+            Vertex v2View = MV.multiply(tri.getV2());
+            Vertex v3View = MV.multiply(tri.getV3());
 
-            if (shouldCullTriangle(v1, v2, v3)) continue;
+            if (shouldCullTriangle(v1View, v2View, v3View)) continue;
 
-            v1 = P.multiply(v1);
+            // Calculate lighting in view space
+            Color shadedColor = flatShading(v1View, v2View, v3View, tri.getColor());
+
+            // Transform to clip space
+            Vertex v1 = P.multiply(v1View);
             v1 = new Vertex(v1.getX()/v1.getW(), v1.getY()/v1.getW(), v1.getZ()/v1.getW(), 1);
-            v2 = P.multiply(v2);
+            Vertex v2 = P.multiply(v2View);
             v2 = new Vertex(v2.getX()/v2.getW(), v2.getY()/v2.getW(), v2.getZ()/v2.getW(), 1);
-            v3 = P.multiply(v3);
+            Vertex v3 = P.multiply(v3View);
             v3 = new Vertex(v3.getX()/v3.getW(), v3.getY()/v3.getW(), v3.getZ()/v3.getW(), 1);
 
+            // Convert to screen space
             v1 = new Vertex(v1.getX() * getWidth()/2 + (double) getWidth() /2, v1.getY() * getHeight()/2 + (double) getHeight() /2, v1.getZ(), 1);
             v2 = new Vertex(v2.getX() * getWidth()/2 + (double) getWidth() /2, v2.getY() * getHeight()/2 + (double) getHeight() /2, v2.getZ(), 1);
             v3 = new Vertex(v3.getX() * getWidth()/2 + (double) getWidth() /2, v3.getY() * getHeight()/2 + (double) getHeight() /2, v3.getZ(), 1);
 
-
-            rasterizeTriangle(g2, zBuffer, v1, v2, v3, tri.getColor());
+            rasterizeTriangle(g2, zBuffer, v1, v2, v3, shadedColor);
         }
         g2.drawImage(img, 0, 0, null);
     }
@@ -166,6 +172,22 @@ public class Canvas extends JPanel{
         double w3 = 1 - w1 - w2;
 
         return new double[]{w1,w2,w3};
+    }
+
+    private Color flatShading(Vertex v1, Vertex v2, Vertex v3, Color color){
+        Vertex edge1 = v2.subtract(v1);
+        Vertex edge2 = v3.subtract(v1);
+        Vertex normal = edge1.cross(edge2).normalize();
+
+        double diffuse = Math.max(0, normal.dot(lightDir));
+        double ambient = 0.2;
+        double intensity = ambient + (1.0 - ambient) * diffuse;
+
+        int r = (int)(color.getRed() * intensity);
+        int g = (int)(color.getGreen() * intensity);
+        int b = (int)(color.getBlue() * intensity);
+
+        return new Color(r, g, b);
     }
 
     public int getRotX() {
