5cf3e39f280c9daf8ae99a21abddf22b56b9c41d
AdhamG <adhamgaweesh@gmail.com>
2026-01-30 14:29:10 +0200

    uploading testing files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 void main(){
     Mesh mesh = OBJParser.parseFile("test_files/suzanne.obj");
 
-    Vertex location = new Vertex(0,0,5,1);
+    Vertex location = new Vertex(0,0,4,1);
     Vertex look = new Vertex(0,0,0,1);
     Vertex up = new Vertex(0,1,0,1);
     Camera camera = new Camera(location, look, up, 75, 0.1, 1000);
